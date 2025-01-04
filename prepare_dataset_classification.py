46d235ae3da02cb3574e9b11b18c75e7fa8cb5b3
Riccardo369 <riccard.marcaccio@studenti.unicam.it>
2025-01-04 18:44:57 +0100

    Refresh dataset classification

--- prepare_dataset_classification.py
+++ prepare_dataset_classification.py
@@ -10,16 +10,13 @@ features_to_mantain = set(['points', 'uci_points', 'length', 'climb_total',
        'profile', 'startlist_quality',        
        'cyclist_age', 'delta', 'label'])
 
-TR = dataset[dataset["race_year"] >= 2022]
-TS = dataset[dataset["race_year"] < 2022]
+TR = dataset[dataset["race_year"] < 2022]
+TS = dataset[dataset["race_year"] >= 2022]
 
 for i in set(dataset.keys()) - features_to_mantain:
     
     del TR[i]
     del TS[i]
-
-TR.info()
-TS.info()
 
 TR.to_csv("dataset_classification/TrainDataset.csv")
 TS.to_csv("dataset_classification/TestDataset.csv")
