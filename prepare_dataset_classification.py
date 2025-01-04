dc3c3cc6c510738c8e2ec581775dcc16a339c673
Riccardo369 <riccard.marcaccio@studenti.unicam.it>
2025-01-04 16:51:01 +0100

    .

--- prepare_dataset_classification.py
+++ prepare_dataset_classification.py
@@ -2,11 +2,24 @@ import pandas as pd
 
 dataset = pd.read_csv("dataset/races_filled.csv")
 
-dataset["label"] = (dataset["position"] >= 20).astype(int)
+dataset["label"] = (dataset["position"] >= 20).astype(float)
+dataset["points"] = dataset["points"].astype(float)
+dataset["startlist_quality"] = dataset["startlist_quality"].astype(float)
+
+features_to_mantain = set(['points', 'uci_points', 'length', 'climb_total',      
+       'profile', 'startlist_quality',        
+       'cyclist_age', 'delta', 'label'])
 
 TR = dataset[dataset["race_year"] >= 2022]
-
 TS = dataset[dataset["race_year"] < 2022]
+
+for i in set(dataset.keys()) - features_to_mantain:
+    
+    del TR[i]
+    del TS[i]
+
+TR.info()
+TS.info()
 
 TR.to_csv("dataset_classification/TrainDataset.csv")
 TS.to_csv("dataset_classification/TestDataset.csv")
